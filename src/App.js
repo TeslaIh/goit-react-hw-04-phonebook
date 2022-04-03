3c84821c69fd4fdaee7fc34f642d17f123816917
TeslaIh <87144117+TeslaIh@users.noreply.github.com>
2022-04-03 11:59:46 +0300

    fourth try
    fixed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import ContactForm from './components/Form/Form';
 import ContactItems from './components/ContactItems/ContactItems';
 import Filter from './components/Filter/Filter';
 
+  const INITIAL_STATE = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
 export default function App() {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
     const storageContacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(storageContacts);
+    const parsedContacts = JSON.parse(storageContacts) ?? INITIAL_STATE;
     setContacts(parsedContacts);
   }, []);
 
